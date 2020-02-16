e43b5b38917f185ae15be831e552c29fcc42d2e5
Julian Alimin <dmastag@yahoo.com>
2020-02-16 13:45:35 +0700

    Manipulasi Gambar dengan Jimp

--- index.js
+++ index.js
@@ -36,6 +36,43 @@ app.get('/image/:fileName', (req, res) => {
   res.sendFile(file);
 })
 
+app.get('/imageJimpWatermark/:fileName', async (req, res) => {
+  const Jimp = require("jimp");
+
+  const ORIGINAL_IMAGE = `${uploadFolder}/${req.params.fileName}`;
+
+  const LOGO = "public/channel-logo.jpg";
+
+  const LOGO_MARGIN_PERCENTAGE = 5;
+
+  const [image, logo] = await Promise.all([
+    Jimp.read(ORIGINAL_IMAGE),
+    Jimp.read(LOGO)
+  ]);
+
+  logo.resize(image.bitmap.width / 10, Jimp.AUTO);
+
+  const xMargin = (image.bitmap.width * LOGO_MARGIN_PERCENTAGE) / 100;
+  const yMargin = (image.bitmap.width * LOGO_MARGIN_PERCENTAGE) / 100;
+
+  const X = image.bitmap.width - logo.bitmap.width - xMargin;
+  const Y = image.bitmap.height - logo.bitmap.height - yMargin;
+
+  image
+    .composite(logo, X, Y, [
+      {
+        mode: Jimp.BLEND_SCREEN,
+        opacitySource: 0.1,
+        opacityDest: 1
+      }
+    ])
+    .getBuffer(Jimp.MIME_JPEG, (err, buffer) => {
+      res.set("Content-Type", Jimp.MIME_JPEG)
+      res.send(buffer)
+    })
+
+})
+
 app.post('/uploadbase64', (req, res) => {
 
   const base64Data = req.body.image.replace(/^data:image\/png;base64,/, "")
